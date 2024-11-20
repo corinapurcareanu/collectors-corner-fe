23c3bc7492c8661911d49a4a638e7d830a3fe0bd
corinapurcareanu <coripurcareanu@gmail.com>
2024-11-20 18:40:56 +0200

    fix footer

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -47,7 +47,7 @@ export const Header = ({cartCount, setCartCount}) => {
                             onChange={(event) => setKey(event.target.value)}
                         />
                         <button type="submit">
-                            <svg id="search-logo" xmlns="http://www.w3.org/2000/svg" width="17" height="57" fill="currentColor" className="bi bi-search" viewBox="0 0 16 18"> 
+                            <svg id="search-logo" xmlns="http://www.w3.org/2000/svg" width="17" height="57" fill="#333;" className="bi bi-search" viewBox="0 0 16 18"> 
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" id="mainIconPathAttribute"></path>
                             </svg>
                         </button>
@@ -62,7 +62,7 @@ export const Header = ({cartCount, setCartCount}) => {
                             onChange={(event) => setKey(event.target.value)}
                         />
                         <button type="submit">
-                            <svg id="search-logo" xmlns="http://www.w3.org/2000/svg" width="17" height="57" fill="currentColor" className="bi bi-search" viewBox="0 0 16 18"> 
+                            <svg id="search-logo" xmlns="http://www.w3.org/2000/svg" width="17" height="57" fill="#333;" className="bi bi-search" viewBox="0 0 16 18"> 
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" id="mainIconPathAttribute"></path>
                             </svg>
                         </button>
